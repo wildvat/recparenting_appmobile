b6ec7222785814095adeaa76f454e756777a7b3b
Jose Suarez <chua.jose@gmail.com>
2023-09-12 15:21:04 +0200

    [add] providers patient and therapist

---
+++ lib/src/patient/providers/patient.provider.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+import 'package:recparenting/src/patient/models/patient.model.dart';
+import 'package:recparenting/src/therapist/models/therapist.model.dart';
+import 'dart:developer' as developer;
+import 'package:recparenting/_shared/models/user.model.dart';
+import 'package:recparenting/_shared/providers/http.dart';
+import 'package:recparenting/src/auth/repository/token_respository.dart';
+import 'package:recparenting/src/current_user/current_user.repository.dart';
+
+class PatientApi {
+  late User user;
+  AuthApiHttp client = AuthApiHttp();
+
+  final CurrentUserRepository _currentUserRepository = CurrentUserRepository();
+  final TokenRepository _tokenRepository = TokenRepository();
+
+  Future<User?> getAll() async {
+    const String endpoint = 'patient';
+    try {
+      Response response = await client.dio.get(endpoint);
+      if (response.statusCode == 200) {
+        User user = User.fromJson(response.data);
+        if(user.type=='patient'){
+          user = Patient.fromJson(response.data);
+        }
+        if(user.type=='therapist'){
+          user = Therapist.fromJson(response.data);
+        }
+        _currentUserRepository.setPreferences(user);
+        return user;
+      } else {
+        _currentUserRepository.clearCurrentUser();
+        _tokenRepository.clearTokens();
+        return null;
+      }
+    } on DioException catch (e) {
+      developer.log('/** ERROR CurrentUserApi.getUser **/');
+      developer.log(e.response.toString());
+      _currentUserRepository.clearCurrentUser();
+      _tokenRepository.clearTokens();
+      return null;
+    }
+  }
+
+}
