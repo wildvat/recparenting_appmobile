378a1b6f661d896fce7d161e530525cfdb389872
cristoj <cristojvt@gmail.com>
2023-09-25 16:41:20 +0000

    rec colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,43 +5,62 @@ import 'package:recparenting/constants/colors.dart';
 class RecThemeData {
   static ThemeData get lightTheme {
     return ThemeData(
-        useMaterial3: true,
-        pageTransitionsTheme: PageTransitionsTheme(
-          builders: {
-            for (final platform in TargetPlatform.values)
-              platform: const NoTransitionsBuilder(),
-          },
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-          bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
-        ),
-        elevatedButtonTheme: const ElevatedButtonThemeData(
-            style: ButtonStyle(
-                foregroundColor: MaterialStatePropertyAll(Colors.white),
-                backgroundColor: MaterialStatePropertyAll(colorRecLight))),
-        textButtonTheme: const TextButtonThemeData(
-            style: ButtonStyle(
-                foregroundColor: MaterialStatePropertyAll(colorRecLight))),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-            shape: CircleBorder(),
-            backgroundColor: colorRecLight,
-            foregroundColor: Colors.white),
-        appBarTheme: const AppBarTheme(
-          color: colorRecDark,
-          foregroundColor: Colors.white,
-          titleTextStyle: TextStyle(fontSize: 18),
-          iconTheme: IconThemeData(color: Colors.white),
-        ),
-        tabBarTheme: const TabBarTheme(
-            labelColor: Colors.white,
-            indicatorColor: Colors.white,
-            indicatorSize: TabBarIndicatorSize.tab,
-            indicator: UnderlineTabIndicator(
-                // color for indicator (underline)
-                borderSide: BorderSide(color: Colors.white))),
-        drawerTheme: const DrawerThemeData(backgroundColor: colorRecDark),
-        brightness: Brightness.light);
+            useMaterial3: true,
+            //colorScheme: ColorScheme(brightness: Brightness.light, primary: colorRec, onPrimary: Colors.white, secondary: colorRecLight, onSecondary: colorRecDark)
+            pageTransitionsTheme: PageTransitionsTheme(
+              builders: {
+                for (final platform in TargetPlatform.values)
+                  platform: const NoTransitionsBuilder(),
+              },
+            ),
+            textSelectionTheme:
+                const TextSelectionThemeData(cursorColor: colorRecLight),
+            textTheme: const TextTheme(
+              displayLarge:
+                  TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
+            ),
+            inputDecorationTheme: const InputDecorationTheme(
+                focusColor: colorRecLight,
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      style: BorderStyle.solid, color: colorRecLight),
+                ),
+                activeIndicatorBorder: BorderSide(color: colorRecLight),
+                labelStyle: TextStyle(color: colorRecDark),
+                border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: colorRecLight))),
+            elevatedButtonTheme: const ElevatedButtonThemeData(
+                style: ButtonStyle(
+                    foregroundColor: MaterialStatePropertyAll(Colors.white),
+                    backgroundColor: MaterialStatePropertyAll(colorRecLight))),
+            textButtonTheme: const TextButtonThemeData(
+                style: ButtonStyle(
+                    foregroundColor: MaterialStatePropertyAll(colorRecLight))),
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+                shape: CircleBorder(),
+                backgroundColor: colorRecLight,
+                foregroundColor: Colors.white),
+            appBarTheme: const AppBarTheme(
+              color: colorRecDark,
+              foregroundColor: Colors.white,
+              titleTextStyle: TextStyle(fontSize: 18),
+              iconTheme: IconThemeData(color: Colors.white),
+            ),
+            tabBarTheme: const TabBarTheme(
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.white24,
+                indicatorColor: Colors.white,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicator: UnderlineTabIndicator(
+                    // color for indicator (underline)
+                    borderSide: BorderSide(color: Colors.white))),
+            drawerTheme: const DrawerThemeData(backgroundColor: colorRecDark),
+            brightness: Brightness.light)
+        .copyWith(
+            colorScheme: ThemeData()
+                .colorScheme
+                .copyWith(primary: colorRecLight, onPrimary: Colors.black));
   }
 
   static ThemeData get darkTheme {
