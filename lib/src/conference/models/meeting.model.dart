53ffbdaa749606c7e7f83878cd56d70e2f0f6d97
cristoj <cristojvt@gmail.com>
2023-10-03 17:12:22 +0000

    remove todo

--- lib/src/conference/models/meeting.model.dart
+++ lib/src/conference/models/meeting.model.dart
@@ -4,10 +4,10 @@ class Meeting {
   final String mediaRegion;
   final MediaPlacement mediaPlacement;
 
-  Meeting(this.meetingId, this.externalMeetingId, this.mediaRegion, this.mediaPlacement);
+  Meeting(this.meetingId, this.externalMeetingId, this.mediaRegion,
+      this.mediaPlacement);
 
   factory Meeting.fromJson(Map<String, dynamic> json) {
-    // TODO zmauricv: Look into JSON Serialization Solutions
     var meetingMap = json['Meeting'];
     return Meeting(
       meetingMap['MeetingId'],
@@ -24,11 +24,15 @@ class MediaPlacement {
   final String signalingUrl;
   final String turnControllerUrl;
 
-  MediaPlacement(this.audioHostUrl, this.audioFallbackUrl, this.signalingUrl, this.turnControllerUrl);
+  MediaPlacement(this.audioHostUrl, this.audioFallbackUrl, this.signalingUrl,
+      this.turnControllerUrl);
 
   factory MediaPlacement.fromJson(Map<String, dynamic> json) {
     var mediaPlacementMap = json['Meeting']['MediaPlacement'];
-    return MediaPlacement(mediaPlacementMap['AudioHostUrl'], mediaPlacementMap['AudioFallbackUrl'],
-        mediaPlacementMap['SignalingUrl'], mediaPlacementMap['TurnControlUrl']);
+    return MediaPlacement(
+        mediaPlacementMap['AudioHostUrl'],
+        mediaPlacementMap['AudioFallbackUrl'],
+        mediaPlacementMap['SignalingUrl'],
+        mediaPlacementMap['TurnControlUrl']);
   }
 }
