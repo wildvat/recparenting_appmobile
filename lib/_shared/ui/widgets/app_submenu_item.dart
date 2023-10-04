ec65afd2c8fd2a58db2b243724b85fa60417087f
cristoj <cristojvt@gmail.com>
2023-10-04 10:23:39 +0000

    mockup montse

--- lib/_shared/ui/widgets/app_submenu_item.dart
+++ lib/_shared/ui/widgets/app_submenu_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recparenting/_shared/models/text_colors.enum.dart';
+import 'package:recparenting/_shared/ui/widgets/title.widget.dart';
 
 class AppSubmenuItemWidget extends StatelessWidget {
   final String titleUrl;
@@ -10,8 +12,14 @@ class AppSubmenuItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MenuItemButton(
       onPressed: onPress,
-      child: Text(titleUrl,
-          style: const TextStyle(color: Colors.white, fontSize: 14)),
+      child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        TitleDefault(titleUrl, color: TextColors.white),
+        Icon(
+          Icons.arrow_outward_outlined,
+          size: 15,
+          color: TextColors.white.color,
+        )
+      ]),
     );
   }
 }
