ed7cbbc609c8da475b973a2c129b80e9cc78088a
cristoj <cristojvt@gmail.com>
2023-10-04 14:38:11 +0000

    add red color

--- lib/_shared/ui/widgets/snack_bar.widget.dart
+++ lib/_shared/ui/widgets/snack_bar.widget.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:recparenting/_shared/models/text_colors.enum.dart';
 import 'package:recparenting/navigator_key.dart';
 
 import '../../providers/r_language.dart';
 
 class SnackBarRec {
   final String message;
-  Color backgroundColor = Colors.red;
+  Color? backgroundColor;
   bool showCloseIcon = true;
   Duration duration;
   final Function()? onPress;
 
   SnackBarRec(
       {required this.message,
-      this.backgroundColor = Colors.red,
+      this.backgroundColor,
       this.showCloseIcon = true,
       this.duration = const Duration(seconds: 20),
-      this.onPress
-      }
-) {
-    scaffoldKey.currentState?.showSnackBar(SnackBar(
-        padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
+      this.onPress}) {
+    scaffoldKey.currentState?.showSnackBar(
+      SnackBar(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
         duration: duration,
         elevation: 5,
         behavior: SnackBarBehavior.floating,
-        backgroundColor: backgroundColor,
+        backgroundColor: backgroundColor ?? TextColors.danger.color,
         content: Text(message.toString()),
         showCloseIcon: showCloseIcon,
-        action: onPress!=null ? SnackBarAction(
-          textColor: Colors.black,
-          label: R.string.generalShow,
-          onPressed: onPress!,
-        ):null,
-    ),
-
+        action: onPress != null
+            ? SnackBarAction(
+                textColor: Colors.black,
+                label: R.string.generalShow,
+                onPressed: onPress!,
+              )
+            : null,
+      ),
     );
   }
 }
