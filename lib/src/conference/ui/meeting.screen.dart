25c68d36ee92d63c9bb7ae3e8cf103d9a767f426
Jose Suarez <chua.jose@gmail.com>
2023-10-05 15:20:01 +0200

    [update] center name therapist and texto waiting therapist camera

--- lib/src/conference/ui/meeting.screen.dart
+++ lib/src/conference/ui/meeting.screen.dart
@@ -60,7 +60,7 @@ class _MeetingScreenState extends State<MeetingScreen> {
                   child: showLocalVideoTile(meetingProvider, context),
                 ),
                 Positioned(
-                  bottom: 10,
+                  bottom: 20,
                   child: localListInfo(meetingProvider, context),
                 ),
                 WillPopScope(
@@ -68,7 +68,7 @@ class _MeetingScreenState extends State<MeetingScreen> {
                     meetingProvider.stopMeeting();
                     return true;
                   },
-                  child: const Spacer(),
+                  child: const SizedBox.shrink(),
                 ),
               ],
             )));
@@ -84,37 +84,45 @@ class _MeetingScreenState extends State<MeetingScreen> {
     developer.log('paramsVT $paramsVT');
     Widget videoTile;
     if (Platform.isIOS) {
-      videoTile = UiKitView(
+      videoTile = SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child:  UiKitView(
         viewType: "videoTile",
         creationParams: paramsVT,
         creationParamsCodec: const StandardMessageCodec(),
-      );
+      ));
     } else if (Platform.isAndroid) {
-      videoTile = PlatformViewLink(
-        viewType: 'videoTile',
-        surfaceFactory:
-            (BuildContext context, PlatformViewController controller) {
-          return AndroidViewSurface(
-            controller: controller as AndroidViewController,
-            gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
-            hitTestBehavior: PlatformViewHitTestBehavior.transparent,
-          );
-        },
-        onCreatePlatformView: (PlatformViewCreationParams params) {
-          final AndroidViewController controller =
-              PlatformViewsService.initExpensiveAndroidView(
-            id: params.id,
-            viewType: 'videoTile',
-            layoutDirection: TextDirection.ltr,
-            creationParams: paramsVT,
-            creationParamsCodec: const StandardMessageCodec(),
-          );
-          controller
-              .addOnPlatformViewCreatedListener(params.onPlatformViewCreated);
-          controller.create();
-          return controller;
-        },
+      videoTile = SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child:  PlatformViewLink(
+          viewType: 'videoTile',
+          surfaceFactory:
+              (BuildContext context, PlatformViewController controller) {
+            return AndroidViewSurface(
+              controller: controller as AndroidViewController,
+              gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+              hitTestBehavior: PlatformViewHitTestBehavior.transparent,
+            );
+          },
+          onCreatePlatformView: (PlatformViewCreationParams params) {
+            final AndroidViewController controller =
+            PlatformViewsService.initExpensiveAndroidView(
+              id: params.id,
+              viewType: 'videoTile',
+              layoutDirection: TextDirection.ltr,
+              creationParams: paramsVT,
+              creationParamsCodec: const StandardMessageCodec(),
+            );
+            controller
+                .addOnPlatformViewCreatedListener(params.onPlatformViewCreated);
+            controller.create();
+            return controller;
+          },
+        ),
       );
+
     } else {
       videoTile =
           TextDefault(AppLocalizations.of(context)!.mettingErrorPlatform);
@@ -268,9 +276,9 @@ class _MeetingScreenState extends State<MeetingScreen> {
       AppLocalizations.of(context)!.meetingWaitingToCamera(
           _currentUser.isPatient() ? 'therapist' : 'patient'),
       size: TitleSize.large,
+      textAlign: TextAlign.center,
     );
     return Center(
-      widthFactor: 2.5,
       child: emptyVideos,
     );
   }
