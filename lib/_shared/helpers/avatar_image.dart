126687402f553ef03c3bdd71a3bfd92435def029
Jose Suarez <chua.jose@gmail.com>
2023-10-10 09:47:58 +0200

    [update] avartar image error icon

--- lib/_shared/helpers/avatar_image.dart
+++ lib/_shared/helpers/avatar_image.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:recparenting/constants/colors.dart';
 
 import '../models/user.model.dart';
 
@@ -15,15 +15,16 @@ class AvatarImage extends StatefulWidget {
 class _AvatarImageState extends State<AvatarImage> {
   @override
   Widget build(BuildContext context) {
+
     return ClipRRect(
         borderRadius: BorderRadius.circular(50.0),
         child: Image.network(widget.user.avatar, fit: BoxFit.cover, width: 48,
             //extensions like .jpg, .png etc
             errorBuilder: (context, error, stackTrace) {
-          return SvgPicture.network(
-            width: 48,
-            fit: BoxFit.cover,
-            widget.user.avatar, // for .svg extension
+          return const Icon(
+              CupertinoIcons.person_circle_fill,
+              size: 48,
+              color: colorRecLight
           );
         }));
   }
