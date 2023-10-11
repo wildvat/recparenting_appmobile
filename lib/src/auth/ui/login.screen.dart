1b8e1b76e3fcbcc68f0393b5e249dcc53072a26a
cristoj <cristojvt@gmail.com>
2023-10-11 14:53:58 +0000

    image background

--- lib/src/auth/ui/login.screen.dart
+++ lib/src/auth/ui/login.screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:recparenting/_shared/models/text_colors.enum.dart';
 import 'package:recparenting/_shared/models/webpage_arguments.dart';
 import 'package:recparenting/_shared/ui/widgets/text.widget.dart';
 import 'package:recparenting/constants/colors.dart';
@@ -53,109 +54,147 @@ class _LoginScreenState extends State<LoginScreen> {
     //_passwordEditingController.text = 'password';
     return Scaffold(
         backgroundColor: colorRec,
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SvgPicture.asset(
-              'assets/images/rec-logo-inverse.svg',
-              width: 80,
-            ),
-            Padding(
-                padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        controller: _emailEditingController,
-                        keyboardType: TextInputType.emailAddress,
+        body: Container(
+            decoration: const BoxDecoration(
+                color: Colors.black26,
+                image: DecorationImage(
+                    image: AssetImage('assets/images/home.jpg'),
+                    fit: BoxFit.cover)),
+            child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/images/rec-logo-inverse.svg',
+                      width: 80,
+                    ),
+                    Padding(
+                        padding:
+                            const EdgeInsets.only(top: 20, left: 20, right: 20),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                controller: _emailEditingController,
+                                keyboardType: TextInputType.emailAddress,
 
-                        // The validator receives the text that the user has entered.
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return AppLocalizations.of(context)!
-                                .generalFormErrorEmail;
-                          }
-                          return null;
-                        },
-                        style: const TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          enabledBorder: const UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                          hintText: AppLocalizations.of(context)!.generalEmail,
-                          hintStyle: const TextStyle(color: Colors.white60),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                          controller: _passwordEditingController,
-                          obscureText: true,
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return AppLocalizations.of(context)!
-                                  .generalFormRequired;
-                            }
-                            return null;
-                          },
-                          style: const TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            enabledBorder: const UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white)),
-                            hintText:
-                                AppLocalizations.of(context)!.generalPassword,
-                            hintStyle: const TextStyle(color: Colors.white60),
-                          )),
-                      const SizedBox(height: 30),
-                      _isLoading
-                          ? const CircularProgressIndicator(
-                              color: Colors.white,
-                            )
-                          : ElevatedButton(
-                              onPressed: () async {
-                                setState(() {
-                                  _isLoading = true;
-                                });
-                                User? response = await AuthApi().login(
-                                    email: _emailEditingController.text,
-                                    password: _passwordEditingController.text);
-                                setState(() {
-                                  _isLoading = false;
-                                });
-                                if (!mounted) return;
-                                if (response != null) {
-                                  getPermissionPushApp();
-                                  Navigator.pushNamedAndRemoveUntil(
-                                      context,
-                                      homeRoute,
-                                      (Route<dynamic> route) => false);
-                                  return;
-                                } else {
-                                  SnackBarRec(
-                                      message: AppLocalizations.of(context)!
-                                          .generalFormLoginError);
-                                }
-                              },
-                              child: SizedBox(
-                                  width: double.infinity,
-                                  child: TextDefault(
-                                    AppLocalizations.of(context)!.generalAccess,
-                                    textAlign: TextAlign.center,
-                                  ))),
-                      const SizedBox(height: 10),
-                      TextButton(
-                          onPressed: () => Navigator.pushNamed(
-                              context, webPageRoute,
-                              arguments: WebpageArguments(
-                                  url:
-                                      'https://app.recparenting.com/password/reset',
-                                  title: AppLocalizations.of(context)!
-                                      .recoveryPassLinnk)),
-                          child: TextDefault(
-                              AppLocalizations.of(context)!.recoveryPassLinnk))
-                    ],
-                  ),
-                ))
-          ]),
-        ));
+                                // The validator receives the text that the user has entered.
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return AppLocalizations.of(context)!
+                                        .generalFormErrorEmail;
+                                  }
+                                  return null;
+                                },
+                                style: const TextStyle(color: Colors.white),
+                                decoration: InputDecoration(
+                                  fillColor: Colors.black26,
+                                  filled: true,
+                                  enabledBorder: const UnderlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.white)),
+                                  hintText: AppLocalizations.of(context)!
+                                      .generalEmail,
+                                  hintStyle:
+                                      const TextStyle(color: Colors.white60),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              TextFormField(
+                                  controller: _passwordEditingController,
+                                  obscureText: true,
+                                  // The validator receives the text that the user has entered.
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return AppLocalizations.of(context)!
+                                          .generalFormRequired;
+                                    }
+                                    return null;
+                                  },
+                                  style: const TextStyle(color: Colors.white),
+                                  decoration: InputDecoration(
+                                    fillColor: Colors.black26,
+                                    filled: true,
+                                    enabledBorder: const UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.white)),
+                                    hintText: AppLocalizations.of(context)!
+                                        .generalPassword,
+                                    hintStyle:
+                                        const TextStyle(color: Colors.white60),
+                                  )),
+                              const SizedBox(height: 30),
+                              _isLoading
+                                  ? const CircularProgressIndicator(
+                                      color: Colors.white,
+                                    )
+                                  : ElevatedButton(
+                                      onPressed: () async {
+                                        setState(() {
+                                          _isLoading = true;
+                                        });
+                                        User? response = await AuthApi().login(
+                                            email: _emailEditingController.text,
+                                            password: _passwordEditingController
+                                                .text);
+                                        setState(() {
+                                          _isLoading = false;
+                                        });
+                                        if (!mounted) return;
+                                        if (response != null) {
+                                          getPermissionPushApp();
+                                          Navigator.pushNamedAndRemoveUntil(
+                                              context,
+                                              homeRoute,
+                                              (Route<dynamic> route) => false);
+                                          return;
+                                        } else {
+                                          SnackBarRec(
+                                              message:
+                                                  AppLocalizations.of(context)!
+                                                      .generalFormLoginError);
+                                        }
+                                      },
+                                      child: SizedBox(
+                                          width: double.infinity,
+                                          child: TextDefault(
+                                            AppLocalizations.of(context)!
+                                                .generalAccess,
+                                            textAlign: TextAlign.center,
+                                          ))),
+                              const SizedBox(height: 10),
+                              TextButton(
+                                  onPressed: () => Navigator.pushNamed(
+                                      context, webPageRoute,
+                                      arguments: WebpageArguments(
+                                          url:
+                                              'https://app.recparenting.com/password/reset',
+                                          title: AppLocalizations.of(context)!
+                                              .recoveryPassLinnk)),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      TextDefault(
+                                        AppLocalizations.of(context)!
+                                            .recoveryPassLinnk,
+                                        color: TextColors.white,
+                                      ),
+                                      const Padding(
+                                        padding:
+                                            EdgeInsets.only(left: 1, bottom: 8),
+                                        child: Icon(
+                                          Icons.arrow_outward_outlined,
+                                          size: 15,
+                                          color: Colors.white,
+                                        ),
+                                      )
+                                    ],
+                                  ))
+                            ],
+                          ),
+                        ))
+                  ]),
+            )));
   }
 }
