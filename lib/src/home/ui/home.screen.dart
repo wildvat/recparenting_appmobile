8edd611a92490fef8813c88fa87c2f563b4bda9d
cristoj <cristojvt@gmail.com>
2023-10-17 20:32:53 +0000

    unnecesary imports

--- lib/src/home/ui/home.screen.dart
+++ lib/src/home/ui/home.screen.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:recparenting/_shared/bloc/language/language_bloc.dart';
-import 'package:recparenting/_shared/models/text_colors.enum.dart';
-import 'package:recparenting/_shared/models/webpage_arguments.dart';
 import 'package:recparenting/_shared/ui/widgets/scaffold_default.dart';
 import 'package:recparenting/_shared/ui/widgets/title.widget.dart';
 import 'package:recparenting/constants/colors.dart';
-import 'package:recparenting/constants/router_names.dart';
 import 'package:recparenting/src/current_user/bloc/current_user_bloc.dart';
-import 'package:recparenting/src/home/ui/home_card.widget.dart';
 import 'package:recparenting/src/home/ui/home_patient.screen.dart';
 import 'package:recparenting/src/home/ui/home_therapist.screen%20.dart';
-
-import '../../../environments/env.dart';
 import '../../auth/repository/token_respository.dart';
 
 class HomePage extends StatefulWidget {
