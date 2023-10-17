340cb228ee144e8f681bb58d9b4a4f29030e704e
Jose Suarez <chua.jose@gmail.com>
2023-10-17 14:14:49 +0200

    [update] diseño ventana despues de generar evento

--- lib/src/calendar/ui/widgets/event_modal_bottom_sheet.dart
+++ lib/src/calendar/ui/widgets/event_modal_bottom_sheet.dart
@@ -39,97 +39,103 @@ class _EventModalBottomSheetState extends State<EventModalBottomSheet> {
     return Container(
         width: double.infinity,
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextDefault(widget.event.title, fontWeight: FontWeight.bold),
-            Container(
-                decoration: BoxDecoration(
-                  color: widget.event.color,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                margin: const EdgeInsets.only(top: 10),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-                child: TextDefault(AppLocalizations.of(context)!
-                    .eventType(
-                        (widget.event.event! as EventModel).type.name)
-                    .toUpperCase())),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    TextDefault(AppLocalizations.of(context)!.eventFrom,
-                        fontWeight: FontWeight.bold),
-                    const SizedBox(width: 5),
-                    TextDefault(widget.event.startTime.toString().substring(
-                        0, widget.event.startTime.toString().length - 7)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    TextDefault(
-                      AppLocalizations.of(context)!.eventTo,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    const SizedBox(width: 5),
-                    TextDefault(widget.event.endTime.toString().substring(
-                        0, widget.event.endTime.toString().length - 7))
-                  ],
-                )
-              ],
-            ),
-            widget.event.event is EventModel ? ButtonAddCalendar(event: widget.event.event as EventModel, isIcon: false): const SizedBox.shrink(),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    late final Patient patient;
-                    if (_currentUser.isPatient()) {
-                      patient = _currentUser as Patient;
-                    } else {
-                      patient = _eventApi.patient;
-                    }
-                    if (_eventApi.type == AppointmentTypes.appointment_chat) {
-                      Navigator.pushNamed(context, chatRoute,
-                          arguments: patient);
-                    } else if (_eventApi.type == AppointmentTypes.appointment_video) {
-                      Navigator.pushNamed(context, joinConferenceRoute,
-                          arguments: patient.conference);
-                    }
-                  },
-                  child: TextDefault(
-                      AppLocalizations.of(context)!.eventGoSessionBtn),
-                ),
-
-                /// only tehrapist or my owns events can be visible in modal
-                /// then not need to check if visible here
-                ElevatedButton(
-                    onPressed: () async {
-                      setState(() => _loading = true);
-                      bool response =
-                          await CalendarApi().deleteEvent(_eventApi.id);
-                      setState(() => _loading = false);
-                      if (response && mounted) {
-                        Navigator.pop(context, response);
+        child: Stack(children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextDefault(widget.event.title, fontWeight: FontWeight.bold),
+              Container(
+                  decoration: BoxDecoration(
+                    color: widget.event.color,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: const EdgeInsets.only(top: 10),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  child: TextDefault(AppLocalizations.of(context)!
+                      .eventType((widget.event.event! as EventModel).type.name)
+                      .toUpperCase())),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      TextDefault(AppLocalizations.of(context)!.eventFrom,
+                          fontWeight: FontWeight.bold),
+                      const SizedBox(width: 5),
+                      TextDefault(widget.event.startTime.toString().substring(
+                          0, widget.event.startTime.toString().length - 7)),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      TextDefault(
+                        AppLocalizations.of(context)!.eventTo,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      const SizedBox(width: 5),
+                      TextDefault(widget.event.endTime.toString().substring(
+                          0, widget.event.endTime.toString().length - 7))
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      late final Patient patient;
+                      if (_currentUser.isPatient()) {
+                        patient = _currentUser as Patient;
+                      } else {
+                        patient = _eventApi.patient;
+                      }
+                      if (_eventApi.type == AppointmentTypes.appointment_chat) {
+                        Navigator.pushNamed(context, chatRoute,
+                            arguments: patient);
+                      } else if (_eventApi.type ==
+                          AppointmentTypes.appointment_video) {
+                        Navigator.pushNamed(context, joinConferenceRoute,
+                            arguments: patient.conference);
                       }
                     },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: TextColors.danger.color),
-                    child: _loading
-                        ? const CircularProgressIndicator(
-                            color: Colors.white,
-                          )
-                        : TextDefault(
-                            AppLocalizations.of(context)!.generalButtonDelete,
-                          ))
-              ],
-            )
-          ],
-        ));
+                    child: TextDefault(
+                        AppLocalizations.of(context)!.eventGoSessionBtn),
+                  ),
+
+                  /// only tehrapist or my owns events can be visible in modal
+                  /// then not need to check if visible here
+                  ElevatedButton(
+                      onPressed: () async {
+                        setState(() => _loading = true);
+                        bool response =
+                            await CalendarApi().deleteEvent(_eventApi.id);
+                        setState(() => _loading = false);
+                        if (response && mounted) {
+                          Navigator.pop(context, response);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: TextColors.danger.color),
+                      child: _loading
+                          ? const CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : TextDefault(
+                              AppLocalizations.of(context)!.generalButtonDelete,
+                            ))
+                ],
+              )
+            ],
+          ),
+        widget.event.event is EventModel ? Positioned(
+            top: 0,
+            right: 20,
+            child:  ButtonAddCalendar(event: widget.event.event as EventModel, isIcon: true)): const SizedBox.shrink(),
+
+        ]));
   }
 }
