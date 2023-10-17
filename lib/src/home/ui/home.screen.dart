a9ba9737436538a478e3f333cfe7b525573feab6
cristoj <cristojvt@gmail.com>
2023-10-17 19:55:10 +0000

    home therapist closes #17 #18

--- lib/src/home/ui/home.screen.dart
+++ lib/src/home/ui/home.screen.dart
@@ -11,6 +11,8 @@ import 'package:recparenting/constants/colors.dart';
 import 'package:recparenting/constants/router_names.dart';
 import 'package:recparenting/src/current_user/bloc/current_user_bloc.dart';
 import 'package:recparenting/src/home/ui/home_card.widget.dart';
+import 'package:recparenting/src/home/ui/home_patient.screen.dart';
+import 'package:recparenting/src/home/ui/home_therapist.screen%20.dart';
 
 import '../../../environments/env.dart';
 import '../../auth/repository/token_respository.dart';
@@ -47,6 +49,265 @@ class _HomePageState extends State<HomePage> {
         body: BlocBuilder<CurrentUserBloc, CurrentUserState>(
             builder: (context, state) {
           if (state is CurrentUserLoaded) {
+            return Center(
+                child: state.user.isPatient()
+                    ? HomePatientWidget(
+                        user: state.user,
+                        lang: _language,
+                        accessToken: _accessToken)
+                    : HomeTherapistWidget(
+                        lang: _language, accessToken: _accessToken));
+
+            return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: LayoutBuilder(builder: (context, constraints) {
+                  final double width = (constraints.maxWidth / 2) - 30;
+                  final double widthColumn3 = (constraints.maxWidth / 3) - 30;
+                  final double height =
+                      (constraints.maxHeight / 2) - 30 - 90 - 20;
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/rec-logo-inverse.svg',
+                          height: 60,
+                        ),
+                        const SizedBox(height: 20),
+                        Wrap(
+                          children: [
+                            HomeCardWidget(
+                              width: width,
+                              height: height,
+                              onPress: () {
+                                if (state.user.isPatient()) {
+                                  Navigator.pushNamed(context, chatRoute,
+                                      arguments: state.user);
+                                } else {
+                                  Navigator.pushNamed(context, patientsRoute);
+                                }
+                              },
+                              colorBack: TextColors.chat.color,
+                              colorText: TextColors.chatDark,
+                              pathImage: 'dashboard_one-min',
+                              text: state.user.isPatient()
+                                  ? AppLocalizations.of(context)!.menuChat
+                                  : AppLocalizations.of(context)!.menuPatients,
+                            ),
+                            HomeCardWidget(
+                              width: width,
+                              height: height,
+                              onPress: () async {
+                                String url = '${env.web}masterclasses/';
+                                if (_language == 'es') {
+                                  url = '${env.web}es/masterclasses/';
+                                }
+                                url = '${env.url}login-token?redirect_to=$url';
+                                Navigator.pushNamed(context, webPageRoute,
+                                    arguments: WebpageArguments(
+                                        title: AppLocalizations.of(context)!
+                                            .menuMasterclasses,
+                                        url: url,
+                                        token: _accessToken));
+                              },
+                              colorBack: TextColors.masterclass.color,
+                              colorText: TextColors.masterclassDark,
+                              isExternalLink: true,
+                              pathImage: 'dashboard_masterclass-min',
+                              text: 'Calendar',
+                            ),
+                            HomeCardWidget(
+                              width: widthColumn3,
+                              height: height,
+                              onPress: () async {
+                                String url = '${env.web}masterclasses/';
+                                if (_language == 'es') {
+                                  url = '${env.web}es/masterclasses/';
+                                }
+                                url = '${env.url}login-token?redirect_to=$url';
+                                Navigator.pushNamed(context, webPageRoute,
+                                    arguments: WebpageArguments(
+                                        title: AppLocalizations.of(context)!
+                                            .menuMasterclasses,
+                                        url: url,
+                                        token: _accessToken));
+                              },
+                              colorBack: TextColors.masterclass.color,
+                              colorText: TextColors.masterclassDark,
+                              isExternalLink: true,
+                              pathImage: 'dashboard_masterclass-min',
+                              text: 'Calendar',
+                            ),
+                            HomeCardWidget(
+                              width: widthColumn3,
+                              height: height,
+                              onPress: () async {
+                                String url = '${env.web}masterclasses/';
+                                if (_language == 'es') {
+                                  url = '${env.web}es/masterclasses/';
+                                }
+                                url = '${env.url}login-token?redirect_to=$url';
+                                Navigator.pushNamed(context, webPageRoute,
+                                    arguments: WebpageArguments(
+                                        title: AppLocalizations.of(context)!
+                                            .menuMasterclasses,
+                                        url: url,
+                                        token: _accessToken));
+                              },
+                              colorBack: TextColors.masterclass.color,
+                              colorText: TextColors.masterclassDark,
+                              isExternalLink: true,
+                              pathImage: 'dashboard_masterclass-min',
+                              text: 'Calendar',
+                            ),
+                            HomeCardWidget(
+                              width: widthColumn3,
+                              height: height,
+                              onPress: () async {
+                                String url = '${env.web}masterclasses/';
+                                if (_language == 'es') {
+                                  url = '${env.web}es/masterclasses/';
+                                }
+                                url = '${env.url}login-token?redirect_to=$url';
+                                Navigator.pushNamed(context, webPageRoute,
+                                    arguments: WebpageArguments(
+                                        title: AppLocalizations.of(context)!
+                                            .menuMasterclasses,
+                                        url: url,
+                                        token: _accessToken));
+                              },
+                              colorBack: TextColors.masterclass.color,
+                              colorText: TextColors.masterclassDark,
+                              isExternalLink: true,
+                              pathImage: 'dashboard_masterclass-min',
+                              text: AppLocalizations.of(context)!
+                                  .menuMasterclasses,
+                            ),
+                            /*
+                            state.user.isPatient()
+                                ? HomeCardWidget(
+                                    width: width,
+                                    height: height,
+                                    onPress: () async {
+                                      String url = '${env.web}podcast/';
+                                      if (_language == 'es') {
+                                        url = '${env.web}es/podcast/';
+                                      }
+                                      url =
+                                          '${env.url}login-token?redirect_to=$url';
+                                      Navigator.pushNamed(context, webPageRoute,
+                                          arguments: WebpageArguments(
+                                              title:
+                                                  AppLocalizations.of(context)!
+                                                      .menuMasterclasses,
+                                              url: url,
+                                              token: _accessToken));
+                                    },
+                                    colorBack: TextColors.recLight.color,
+                                    colorText: TextColors.rec,
+                                    isExternalLink: true,
+                                    pathImage: 'dashboard_podcast-min',
+                                    text: AppLocalizations.of(context)!
+                                        .menuPodcast,
+                                  )
+                                : const SizedBox.shrink(),
+                            state.user.isPatient()
+                                ? HomeCardWidget(
+                                    width: width,
+                                    height: height,
+                                    onPress: () => Navigator.pushNamed(
+                                        context, webPageRoute,
+                                        arguments: WebpageArguments(
+                                            title: AppLocalizations.of(context)!
+                                                .menuFaqs,
+                                            url:
+                                                'https://www.recparenting.com/faq-users/')),
+                                    colorBack: TextColors.rec.color,
+                                    colorText: TextColors.recLight,
+                                    isExternalLink: true,
+                                    pathImage: 'dashboard_faqs-min',
+                                    text:
+                                        AppLocalizations.of(context)!.menuFaqs,
+                                  )
+                                : const SizedBox.shrink()
+                                */
+                          ],
+                        ),
+                        /*
+                        Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 30),
+                            child: Column(
+                              children: [
+                                ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor:
+                                          TextColors.masterclass.color,
+                                      minimumSize: const Size.fromHeight(50),
+                                    ),
+                                    child: TextDefault(
+                                      AppLocalizations.of(context)!
+                                          .menuMasterclasses,
+                                      color: TextColors.masterclassDark,
+                                    )),
+                                const SizedBox(height: 20),
+                                ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor:
+                                          TextColors.recLight.color,
+                                      minimumSize: const Size.fromHeight(50),
+                                    ),
+                                    child: TextDefault(
+                                      AppLocalizations.of(context)!.menuPodcast,
+                                      color: TextColors.rec,
+                                    )),
+                                const SizedBox(height: 20),
+                                ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: TextColors.rec.color,
+                                      minimumSize: const Size.fromHeight(50),
+                                    ),
+                                    child: TextDefault(
+                                      AppLocalizations.of(context)!.menuFaqs,
+                                      color: TextColors.recLight,
+                                    )),
+                              ],
+                            ))
+                            */
+                      ],
+                    ),
+                    /*
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HomeCardWidget(
+                        onPress: () {
+                          if (state.user.isPatient()) {
+                            Navigator.pushNamed(context, chatRoute,
+                                arguments: state.user);
+                          } else {
+                            Navigator.pushNamed(context, patientsRoute);
+                          }
+                        },
+                        colorBack: TextColors.chat.color,
+                        colorText: TextColors.chatDark,
+                        pathImage: 'dashboard_one-min',
+                        text: state.user.isPatient()
+                            ? AppLocalizations.of(context)!.menuChat
+                            : AppLocalizations.of(context)!.menuPatients,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              */
+                  );
+                }));
+            /*
             return LayoutBuilder(builder: (context, constraints) {
               return Column(
                 children: [
@@ -138,6 +399,7 @@ class _HomePageState extends State<HomePage> {
                 ],
               );
             });
+            */
           }
           return TitleDefault(AppLocalizations.of(context)!.userNotLogged);
         }));
--- lib/src/home/ui/home_card.widget.dart
+++ lib/src/home/ui/home_card.widget.dart
@@ -5,15 +5,20 @@ import 'package:recparenting/_shared/ui/widgets/text.widget.dart';
 
 class HomeCardWidget extends StatelessWidget {
   final String text;
-  final String pathImage;
+  final double? width;
+  final double? height;
+  final String? pathImage;
   final TextColors colorText;
   final Color colorBack;
   final bool isExternalLink;
   final Function() onPress;
+  double? _widthMedia;
 
-  const HomeCardWidget(
+  HomeCardWidget(
       {required this.text,
-      required this.pathImage,
+      this.pathImage,
+      this.width,
+      this.height,
       required this.onPress,
       required this.colorText,
       required this.colorBack,
@@ -22,26 +27,50 @@ class HomeCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (width == null && pathImage != null) {
+      _widthMedia = (MediaQuery.of(context).size.width / 2) - 40;
+    }
+
     return GestureDetector(
-        onTap: onPress,
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Container(
-            clipBehavior: Clip.antiAlias,
-            alignment: Alignment.bottomCenter,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/images/$pathImage.jpg'))),
-            child: Container(
+      onTap: onPress,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+        child: Column(
+          children: [
+            pathImage != null
+                ? Container(
+                    width: width == null && pathImage != null
+                        ? _widthMedia
+                        : width,
+                    height: height == null && pathImage != null
+                        ? (_widthMedia ?? 0) * 1.35
+                        : height,
+                    clipBehavior: Clip.antiAlias,
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15)),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/$pathImage.jpg'))))
+                : const SizedBox.shrink(),
+            Container(
               padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-              width: double.infinity,
-              color: colorBack,
+              width: _widthMedia,
+              decoration: BoxDecoration(
+                  color: colorBack,
+                  borderRadius: (pathImage == null)
+                      ? BorderRadius.circular(15)
+                      : const BorderRadius.only(
+                          bottomLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        )),
               child: Stack(
                 children: [
                   Container(
-                      padding: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 5, vertical: 8),
                       width: double.infinity,
                       child: TextDefault(
                         text,
@@ -52,8 +81,8 @@ class HomeCardWidget extends StatelessWidget {
                       )),
                   isExternalLink
                       ? Positioned(
-                          top: -3,
-                          right: -3,
+                          top: 0,
+                          right: 0,
                           child: Icon(
                             Icons.arrow_outward_outlined,
                             size: 15,
@@ -63,8 +92,10 @@ class HomeCardWidget extends StatelessWidget {
                       : const SizedBox.shrink()
                 ],
               ),
-            ),
-          ),
-        ));
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
