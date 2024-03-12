9c5ec853fdf395b07c725dadf7b667b4350e46d6
cristojv <cristojvt@gmail.com>
2024-03-12 11:06:03 +0100

    new ios platform

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,31 @@
+import AmazonChimeSDK
+import AmazonChimeSDKMedia
+import AVFoundation
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    var methodChannel: MethodChannelCoordinator?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      let controller = window.rootViewController as! FlutterViewController
+            
+      let binaryMessenger = controller.binaryMessenger
+    
+      methodChannel = MethodChannelCoordinator(binaryMessenger: binaryMessenger)
+    
+      methodChannel?.setUpMethodCallHandler()
+    
+      let viewFactory = FlutterVideoTileFactory(messenger: binaryMessenger)
+    
+      registrar(forPlugin: "AmazonChimeSDKFlutterDemo")?.register(viewFactory, withId: "videoTile")
+      FirebaseApp.configure() //add this before the code below
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- ios/Runner/FlutterVideoTileFactory.swift
+++ ios/Runner/FlutterVideoTileFactory.swift
@@ -1,8 +1,32 @@
-//
-//  FlutterVideoTileFactory.swift
-//  Runner
-//
-//  Created by Mikel Eguidazu on 11/3/24.
-//
-
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ */
+ 
+import Flutter
 import Foundation
+
+class FlutterVideoTileFactory: NSObject, FlutterPlatformViewFactory {
+    private let messenger: FlutterBinaryMessenger
+
+    init(messenger: FlutterBinaryMessenger) {
+        self.messenger = messenger
+        super.init()
+    }
+
+    func create(
+        withFrame frame: CGRect,
+        viewIdentifier viewId: Int64,
+        arguments args: Any?
+    ) -> FlutterPlatformView {
+        return VideoTileView(
+            frame: frame,
+            viewIdentifier: viewId,
+            arguments: args
+        )
+    }
+
+    public func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
+        return FlutterStandardMessageCodec.sharedInstance()
+    }
+}
